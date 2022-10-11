04800b4c3a00eda877561c4ec20cae0240732da3
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-11 11:28:30 +0200

    add props

--- lib/src/features/main/domain/entities/weather_entity.dart
+++ lib/src/features/main/domain/entities/weather_entity.dart
@@ -1,4 +1,6 @@
-class Weather{
+import 'package:equatable/equatable.dart';
+
+class Weather extends Equatable{
   final String? city;
   final DateTime time;
   final int temp;
@@ -8,5 +10,8 @@ class Weather{
   final String icon;
   final String countryCode;
 
-  Weather(this.city, this.time, this.temp, this.windSpeed, this.humidity, this.pressure, this.icon, this.countryCode);
+  const Weather(this.city, this.time, this.temp, this.windSpeed, this.humidity, this.pressure, this.icon, this.countryCode);
+
+  @override
+  List<Object?> get props => [city, time, temp, windSpeed, humidity, pressure, icon, countryCode];
 }
