2900f59e10eed9c5c2fae9b3f0195855ddc9b65a
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-11 10:09:47 +0200

    add weather api end points

---
+++ lib/src/core/network/api/api_endpoints.dart
@@ -0,0 +1,6 @@
+class ApiEndPoints{
+  static const String baseUrl = "https://api.openweathermap.org/data/2.5/";
+  static const String apiKey = "cfbf54c9b97246e061430b9fe2d2cdbd";
+  static const String currentWeather = "weather?";
+  static const String forecast = "forecast?";
+}
