7430943e9e42ec0644ec015ff04425d2599e9c0d
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-11 11:44:06 +0200

    add exception strings

--- lib/src/core/utils/strings_manager.dart
+++ lib/src/core/utils/strings_manager.dart
@@ -2,3 +2,13 @@ class AppStrings{
   static const String appTitle = "Weather App";
   static const String noRouteFound = "No Route Found";
 }
+
+class ExceptionStrings{
+  static const String errDuringCommunication = "Error During Communication";
+  static const String badRequest = "Bad Request";
+  static const String unauthorized = "Unauthorized";
+  static const String requestInfoNotFound = "Requested Info Not Found";
+  static const String conflictOccurred = "Conflict Occurred";
+  static const String internalServerError = "Internal Server Error";
+  static const String noInternetConnection = "No Internet Connection";
+}
