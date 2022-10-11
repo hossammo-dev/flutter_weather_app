80d5daf74036405a9bc62b057863e8f8f1145576
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-11 09:44:36 +0200

    get current weather

--- lib/src/features/main/data/data_sources/remote_data_source/remote_data_source.dart
+++ lib/src/features/main/data/data_sources/remote_data_source/remote_data_source.dart
@@ -1,3 +1,6 @@
+import '../../../../../core/network/requests/requests.dart';
+import '../../models/weather_model.dart';
+
 abstract class RemoteDataSource{
-  
+  Future<WeatherModel> getCurrentWeather(CoordObject coordObject);
 }
