6884ee78dd455d54391eee29791c731e832990ed
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-13 09:45:42 +0200

    add app colors

--- lib/src/core/utils/color/colors_manager.dart
+++ lib/src/core/utils/color/colors_manager.dart
@@ -1,3 +1,11 @@
-class ColorsManager{
+import 'package:flutter/material.dart';
 
+import 'hex_color_util..dart';
+
+class ColorsManager{
+static Color primaryColor = HexColor("#1F1E31");
+
+static Color whiteColor = HexColor("#000000");
+static Color blackColor = HexColor("#FFFFFF");
+static Color greyColor = HexColor("#808080");
 }
