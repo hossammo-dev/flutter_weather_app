f171df4671b11898e15081b1053b41426b068a4c
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-13 09:56:04 +0200

    add card theme

--- lib/src/config/theme/app_theme.dart
+++ lib/src/config/theme/app_theme.dart
@@ -1,3 +1,18 @@
 import 'package:flutter/material.dart';
 
-ThemeData get appTheme => ThemeData();
+import '../../core/utils/color/colors_manager.dart';
+import '../../core/utils/values_manager.dart';
+
+ThemeData get appTheme => ThemeData(
+      primaryColor: ColorsManager.primaryColor,
+  scaffoldBackgroundColor: ColorsManager.whiteColor,
+
+  //card theme
+  cardTheme: CardTheme(
+    color: ColorsManager.whiteColor,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(SizeValues.s8)),
+    elevation: SizeValues.s4,
+    clipBehavior: Clip.antiAliasWithSaveLayer,
+    shadowColor: ColorsManager.blackColor,
+  ),
+    );
