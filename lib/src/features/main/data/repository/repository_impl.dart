adb49f79cd4aa58a3f953bf27b1367a41b4ef2c7
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-13 08:13:52 +0200

    set get weather implementation

--- lib/src/features/main/data/repository/repository_impl.dart
+++ lib/src/features/main/data/repository/repository_impl.dart
@@ -1,17 +1,30 @@
 import 'package:dartz/dartz.dart';
 
-import 'package:weather/src/core/network/error/failure.dart';
-
-import 'package:weather/src/core/network/request_objects/request_objects.dart';
-
-import 'package:weather/src/features/main/domain/entities/weather_entity.dart';
-
+import '../../../../core/network/error/failure.dart';
+import '../../../../core/network/network_info.dart';
+import '../../../../core/network/request_objects/request_objects.dart';
+import '../../domain/entities/weather_entity.dart';
 import '../../domain/repository/repository.dart';
+import '../data_sources/remote_data_source/remote_data_source.dart';
 
 class RepositoryImpl implements Repository{
+  final NetworkInfo _networkInfo;
+  final RemoteDataSource _remoteDataSource;
+  RepositoryImpl(this._networkInfo, this._remoteDataSource);
+
   @override
-  Future<Either<Failure, Weather>> getWeather(CoordObject coordObject) {
-    // TODO: implement getWeather
+  Future<Either<Failure, Weather>> getWeather(CoordObject coordObject) async {
+    if(await _networkInfo.isConnected){
+      try{
+        final response = await _remoteDataSource.getWeather(coordObject);
+        //save data to cache
+        return Right(response);
+      } on ServerFailure {
+        return Left(ServerFailure());
+      }
+    }else{
+      //get data from local
+    }
     throw UnimplementedError();
   }
 
