1754597f2a7bb5a1dcb5fe900be4bed51d6f02bc
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-13 11:01:55 +0200

    fix white and black  colors

--- lib/src/core/utils/color/colors_manager.dart
+++ lib/src/core/utils/color/colors_manager.dart
@@ -5,7 +5,7 @@ import 'hex_color_util..dart';
 class ColorsManager{
 static Color primaryColor = HexColor("#1F1E31");
 
-static Color whiteColor = HexColor("#000000");
-static Color blackColor = HexColor("#FFFFFF");
+static Color whiteColor = HexColor("#FFFFFF");
+static Color blackColor = HexColor("#000000");
 static Color greyColor = HexColor("#808080");
 }
