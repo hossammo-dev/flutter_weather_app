c569873008988b4baf2a0705d6cd0e7593944b2a
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-13 14:59:12 +0200

    add new colors

--- lib/src/core/utils/color/colors_manager.dart
+++ lib/src/core/utils/color/colors_manager.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 
 import 'hex_color_util..dart';
 
-class ColorsManager{
-static Color primaryColor = HexColor("#1F1E31");
-static Color whiteColor = HexColor("#FFFFFF");
-static Color blackColor = HexColor("#000000");
-static Color greyColor = HexColor("#808080");
-static Color blueGreyColor = HexColor("#9AA5B3");
+class ColorsManager {
+  static Color primaryColor = HexColor("#1F1E31");
+  static Color whiteColor = HexColor("#FFFFFF");
+  static Color blackColor = HexColor("#000000");
+  static Color greyColor = HexColor("#808080");
+  static Color lightGreyColor = HexColor("#9AA5B3");
+  static Color lightBlueColor = HexColor("#CAE3FF");
+  static Color lightWhiteColor = HexColor("#DCE0E4");
 }
