08018c23c24edda0fb5faaaa35e862d152fb763e
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-13 07:47:12 +0200

    add condition property

--- lib/src/features/main/domain/entities/weather_entity.dart
+++ lib/src/features/main/domain/entities/weather_entity.dart
@@ -1,17 +1,20 @@
 import 'package:equatable/equatable.dart';
 
-class Weather extends Equatable{
+class Weather extends Equatable {
   final String? city;
-  final DateTime time;
   final int temp;
-  final int windSpeed;
   final int humidity;
   final int pressure;
-  final String icon;
+  final int windSpeed;
+  final String condition;
+  final String iconCode;
   final String countryCode;
+  final DateTime time = DateTime.now();
 
-  const Weather(this.city, this.time, this.temp, this.windSpeed, this.humidity, this.pressure, this.icon, this.countryCode);
+  Weather(this.city, this.temp, this.humidity, this.pressure, this.windSpeed, this.condition,
+      this.iconCode, this.countryCode);
 
   @override
-  List<Object?> get props => [city, time, temp, windSpeed, humidity, pressure, icon, countryCode];
+  List<Object?> get props =>
+      [city, time, temp, windSpeed, humidity, pressure, condition, iconCode, countryCode];
 }
