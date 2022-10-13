a0c4c555a66e954290e6fa0e6e25199f5261211b
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-13 15:10:19 +0200

    build profile view

--- lib/src/features/main/sub_features/profile/presentation/screens/profile_view.dart
+++ lib/src/features/main/sub_features/profile/presentation/screens/profile_view.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ForecastView extends StatelessWidget {
-  const ForecastView({Key? key}) : super(key: key);
+class ProfileView extends StatelessWidget {
+  const ProfileView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Forecast Route"),
+        child: Text("Profile Route"),
       ),
     );
   }
