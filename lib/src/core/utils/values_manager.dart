7ecd585ad861b523fc39d8371b8712575cfc338f
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-13 09:54:36 +0200

    add some default app values

---
+++ lib/src/core/utils/values_manager.dart
@@ -0,0 +1,31 @@
+class PaddingValues{
+  static const double p0 = 0.0;
+  static const double p8 = 8.0;
+  static const double p12 = 12.0;
+  static const double p16 = 16.0;
+  static const double p18 = 18.0;
+  static const double p20 = 20.0;
+  static const double p32 = 32.0;
+}
+
+class MarginValues{
+  static const double m0 = 0.0;
+  static const double m8 = 8.0;
+  static const double m12 = 12.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+  static const double m32 = 32.0;
+}
+
+class SizeValues{
+  static const double s0 = 0.0;
+  static const double s4 = 4.0;
+  static const double s8 = 8.0;
+  static const double s10 = 10.0;
+  static const double s12 = 12.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s32 = 32.0;
+}
