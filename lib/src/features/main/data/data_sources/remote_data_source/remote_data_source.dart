7d2b40746a56e2c1cc6d2a89910ee6746704fbec
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-13 07:49:08 +0200

    implement get current weather function

--- lib/src/features/main/data/data_sources/remote_data_source/remote_data_source.dart
+++ lib/src/features/main/data/data_sources/remote_data_source/remote_data_source.dart
@@ -1,6 +1,21 @@
-import '../../../../../core/network/requests/requests.dart';
+import 'package:weather/src/core/network/api/api_endpoints.dart';
+
+import '../../../../../core/network/request_objects/request_objects.dart';
+import '../../../../../core/network/api/api_services_client.dart';
 import '../../models/weather_model.dart';
 
-abstract class RemoteDataSource{
+abstract class RemoteDataSource {
   Future<WeatherModel> getCurrentWeather(CoordObject coordObject);
+}
+
+class RemoteDataSourceImpl implements RemoteDataSource {
+  final ApiServicesClient _apiClient;
+
+  RemoteDataSourceImpl(this._apiClient);
+
+  @override
+  Future<WeatherModel> getCurrentWeather(CoordObject coordObject) async {
+    final response = await _apiClient.get(ApiEndPoints.currentWeather);
+    return WeatherModel.fromJson(response);
+  }
 }
