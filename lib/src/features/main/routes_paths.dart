24d734a42494da9be07e6039aa70c3a00738fd6d
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-13 15:08:23 +0200

    add main routes paths

---
+++ lib/src/features/main/routes_paths.dart
@@ -0,0 +1,5 @@
+export 'sub_features/weather/presentation/screens/weather_view.dart';
+export 'sub_features/chat/presentation/screens/chat_view.dart';
+export 'sub_features/search/presentation/screens/search_view.dart';
+export 'sub_features/favorite/presentation/screens/favorite_view.dart';
+export 'sub_features/profile/presentation/screens/profile_view.dart';
