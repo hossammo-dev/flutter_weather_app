8987b44848bbd3d0aeb82e535e19c38b2fd3674d
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-16 09:48:19 +0200

    code refactor

--- lib/src/features/main/sub_features/weather/data/models/weather_model.dart
+++ lib/src/features/main/sub_features/weather/data/models/weather_model.dart
@@ -1,27 +1,18 @@
 import '../../domain/entities/weather_entity.dart';
 
 class WeatherModel extends Weather {
-  WeatherModel(String city, int temp, int humidity, int pressure, int windSpeed, String condition,
-      String iconCode, String countryCode)
-      : super(
-      city,
-      temp,
-      humidity,
-      pressure,
-      windSpeed,
-      condition,
-      iconCode,
-      countryCode);
+  WeatherModel(String city, int temp, int humidity, int pressure,
+      double windSpeed, String condition, String iconCode, String countryCode)
+      : super(city, temp, humidity, pressure, windSpeed, condition, iconCode,
+            countryCode);
 
-  factory WeatherModel.fromJson(Map<String, dynamic> json) =>
-      WeatherModel(
-        json['name'],
-        json['weather']['temp'],
-        json['weather']['humidity'],
-        json['weather']['pressure'],
-        json['wind']['speed'],
-        json['weather'][0]['description'],
-        json['weather'][0]['icon'],
-        json['sys']['country']
-      );
+  factory WeatherModel.fromJson(Map<String, dynamic> json) => WeatherModel(
+      json['name'],
+      (json['main']['temp']).round(),
+      json['main']['humidity'],
+      json['main']['pressure'],
+      json['wind']['speed'],
+      json['weather'][0]['description'],
+      json['weather'][0]['icon'],
+      json['sys']['country']);
 }
