82e19c8a17050406e1ac96e5bd15506c3f0acf4b
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-16 11:04:49 +0200

    add getParams method

--- lib/src/core/functions/functions.dart
+++ lib/src/core/functions/functions.dart
@@ -1,5 +1,6 @@
 import '../network/api/api_endpoints.dart';
 
+///a method to get all weather api parameters
 Map<String, dynamic> getParams({double? lat, double? lon, String? cityName}) => {
   ApiQueries.lat : lat ?? 0.0,
   ApiQueries.lon : lon ?? 0.0,
