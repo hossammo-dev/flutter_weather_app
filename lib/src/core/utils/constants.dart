2b478d1b240a5b7d056d8e91762f68788445a08f
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-16 08:00:28 +0200

    code refactor - bgImage

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -1,3 +1,5 @@
 class Constants{
   static const int splashDuration = 3000;
+  static const String bgImage =
+      "https://images.unsplash.com/photo-1532178910-7815d6919875?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80";
 }
--- lib/src/features/main/sub_features/weather/presentation/screens/weather_view.dart
+++ lib/src/features/main/sub_features/weather/presentation/screens/weather_view.dart
@@ -8,15 +8,13 @@ class WeatherView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String bgImage =
-        "https://images.unsplash.com/photo-1532178910-7815d6919875?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80";
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(PaddingValues.p12),
           child: Column(
             children: [
-              Expanded(child: _buildWeatherCard(context, bgImage)),
+              Expanded(child: _buildWeatherCard(context, Constants.bgImage)),
               const SizedBox(height: SizeValues.s20),
               _buildForecast(context),
             ],
