dbdb7d5659d921e3776da7fe5fd140dfa6e8d73d
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-16 11:29:40 +0200

    modify forecast from json method

--- lib/src/features/forecast/data/models/forecast_model.dart
+++ lib/src/features/forecast/data/models/forecast_model.dart
@@ -4,6 +4,6 @@ class ForecastModel extends Forecast {
   const ForecastModel(int temp, int time, String iconCode)
       : super(temp, time, iconCode);
 
-  factory ForecastModel.fromJson(Map<String, dynamic> json) => ForecastModel(
-      (json['main']['temp']).round(), json['dt'], json['weather'][0]['icon']);
+  factory ForecastModel.fromJson(Map<String, dynamic> json) => json['list'].map((jsonObj) => ForecastModel(
+        (jsonObj['main']['temp']).round(), jsonObj['dt'], jsonObj['weather'][0]['icon']));
 }
