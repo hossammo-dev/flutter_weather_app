574c14ca542217c1dc148363ab1d8f7570992663
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-10-22 14:15:25 +0200

    run weather and forecast injectors

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather/bloc_observer.dart';
 
+
 import 'app.dart';
 import 'src/base_injector.dart';
+import 'src/features/forecast/forecast_injector.dart';
+import 'src/features/main/sub_features/weather/weather_injector.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   await initAppModule();
+  await initWeatherModule();
+  await initForecastModule();
   runApp(const WeatherApp());
 }
