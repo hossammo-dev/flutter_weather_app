4faa2b150f2dfa43daa00576e2880f8007acec9c
Hossam Mohammad <info.hossammohammed@gmail.com>
2022-11-14 11:48:34 +0200

    add blue grey color

--- lib/src/core/utils/color/colors_manager.dart
+++ lib/src/core/utils/color/colors_manager.dart
@@ -10,6 +10,7 @@ class ColorsManager {
   static Color lightGreyColor = HexColor("#9AA5B3");
   static Color lightBlueColor = HexColor("#CAE3FF");
   static Color lightWhiteColor = HexColor("#DCE0E4");
+  static Color lightBlueGreyColor = HexColor("#4C596C");
 
   static const Color transparent = Color(0x00000000);
 }
